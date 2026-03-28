fea52c5aff61401c07201785312dea5f06ddcfd3
Makoto-Araki <mj-araki@ivy.ocn.ne.jp>
2026-03-28 13:50:42 +0900

    feature/intro-mypy(#41)-2

--- src/jptext/width.py
+++ src/jptext/width.py
@@ -1,12 +1,11 @@
+from typing import cast
+
 import jaconv
 
 
-# --------------------------------------------------
-# 日本語文字列を半角に正規化
-# --------------------------------------------------
 def to_half_width(text: str) -> str:
     """
-    日本語文字列を半角に正規化する。
+    日本語文字列を半角に正規化
     - 全角カナを半角カナに変換
     - 全角英数字を半角英数字に変換
 
@@ -29,4 +28,4 @@ def to_half_width(text: str) -> str:
     if not isinstance(text, str):
         raise TypeError("text must be str")
 
-    return jaconv.z2h(text, kana=True, ascii=True, digit=True)
+    return cast(str, jaconv.z2h(text, kana=True, ascii=True, digit=True))
